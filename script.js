de7cf5eca4a43225079dfb1f146c804a4d26fe05
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2022-09-30 13:39:12 +0100

    update_before_dark_mode

--- script.js
+++ script.js
@@ -36,3 +36,30 @@ let sprawdz = (form) => {
         return false;
     }
 }
+
+//Drak Mode 
+const button_Switch = document.querySelector('button');
+let theme = localStorage.getItem('theme');
+
+button_Switch.addEventListener('click', () => {
+    if (theme == 'dark') {
+        document.querySelector('body').classList.remove('dark');
+        document.querySelector('body').classList.add('light');
+        theme = 'light';
+    }else {
+        document.querySelector('body').classList.remove('light');
+        document.querySelector('body').classList.add('dark');
+        theme = 'dark';
+    }
+    localStorage.setItem('theme', theme)
+});
+
+if (theme == 'dark') {
+    document.querySelector('body').classList.add('light');
+    theme = 'light';
+}
+
+if (theme == 'light') {
+    document.querySelector('body').classList.add('dark');
+    theme = 'dark';
+}
